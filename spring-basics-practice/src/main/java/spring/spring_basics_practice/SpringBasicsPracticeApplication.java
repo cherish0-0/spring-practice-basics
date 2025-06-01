a27cf29fb2f7bb36cb01a47cf73326f2b1e332dd
Jiyu <159970674+cherish0-0@users.noreply.github.com>
2025-06-01 22:26:58 +0900

    create spring project

---
+++ spring-basics-practice/src/main/java/spring/spring_basics_practice/SpringBasicsPracticeApplication.java
@@ -0,0 +1,13 @@
+package spring.spring_basics_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsPracticeApplication.class, args);
+	}
+
+}
