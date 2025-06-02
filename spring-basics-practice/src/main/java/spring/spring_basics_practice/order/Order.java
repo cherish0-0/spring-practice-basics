2dd886cd05f4ce7f5e7dff16f240f85035f3e6d8
cherish0-0 <jiyu01160@naver.com>
2025-06-02 14:30:50 +0900

    -주문 엔티티 생성

---
+++ spring-basics-practice/src/main/java/spring/spring_basics_practice/order/Order.java
@@ -0,0 +1,52 @@
+package spring.spring_basics_practice.order;
+
+public class Order {
+
+    //fields
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+
+    //constructor
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    //method
+    public int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    //getters
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
+}
