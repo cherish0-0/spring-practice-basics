f0c243be5be15d1569e4e89a7f33c36c4f76abc1
cherish0-0 <jiyu01160@naver.com>
2025-06-02 20:56:20 +0900

    -MemberApp에 스프링 컨테이너 적용

--- spring-basics-practice/src/main/java/spring/spring_basics_practice/MemberApp.java
+++ spring-basics-practice/src/main/java/spring/spring_basics_practice/MemberApp.java
@@ -1,18 +1,30 @@
 package spring.spring_basics_practice;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.spring_basics_practice.member.Grade;
 import spring.spring_basics_practice.member.MemberService;
 import spring.spring_basics_practice.member.MemberServiceImpl;
 
+/**
+ * Application class to demonstrate member registration functionality
+ * This class serves as a simple client for testing the member service using Spring Container
+ */
 public class MemberApp {
     public static void main(String[] args) {
         //Create a new memberService
         //MemberService memberService = new MemberServiceImpl();
 
         //Create AppConfig instance to get configured beans
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
         //Get MemberService from AppConfig (uses dependency injection)
-        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = appConfig.memberService();
+
+        // Using Spring Container with AppConfig
+        // ApplicationContext is the Spring container that manages all beans
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // Get the memberService bean from the Spring container
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         //Create a new test member
         Member member = new Member(1L, "memberA", Grade.VIP);
